9bbf27019e73a5d65b749ce9e8ef9f2657a75e82
rewardru <153069362+rewardru@users.noreply.github.com>
2023-12-06 00:40:26 -0800

    Update setup_zeph_miner.sh

--- setup_zeph_miner.sh
+++ setup_zeph_miner.sh
@@ -246,7 +246,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "br.zephyr.herominers.com:'$PORT'",/' $HOME/c3pool/config.json
+sed -i 's/"url": *"[^"]*",/"url": "us.zephyr.herominers.com:'$PORT'",/' $HOME/c3pool/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/c3pool/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/c3pool/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/c3pool/config.json
